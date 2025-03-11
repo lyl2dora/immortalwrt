852330efc0cb6bf0ee1cf212dc4d862901f241b6
lyl2dora <lyl2dora@gmail.com>
2025-03-12 00:16:51 +0800

    Update diy-part1-x64.sh

--- diy-part1-x64.sh
+++ diy-part1-x64.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=============================================================
 
 # 添加 sirpdboy 的 luci-app-advanced 包
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
