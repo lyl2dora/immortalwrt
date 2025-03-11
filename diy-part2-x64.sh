432152b2bd870bb94fefe94a2a48e424c62e667b
lyl2dora <lyl2dora@gmail.com>
2025-03-12 00:16:31 +0800

    Update diy-part2-x64.sh

--- diy-part2-x64.sh
+++ diy-part2-x64.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=============================================================
 
 # 修改默认 IP
 sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"}/lan) ipad=${ipaddr:-"192.168.3.1"}/g' package/base-files/files/bin/config_generate
@@ -39,6 +31,9 @@ chmod +x package/base-files/files/etc/board.d/99-default_network
 # 更精确地替换 PPPoE 的用户名和密码
 sed -i "/proto='pppoe'/,/password=/ s/username='username'/username='$PPPOE_USERNAME2'/g" package/base-files/files/bin/config_generate
 sed -i "/proto='pppoe'/,/password=/ s/password='password'/password='$PPPOE_PASSWORD2'/g" package/base-files/files/bin/config_generate
+
+#修改默认时区
+sed -i "s/timezone='GMT0'/timezone='Asia\/Shanghai'/" package/base-files/files/bin/config_generate
 
 # 更加精确的替换，包含完整的函数名和上下文
 sed -i '/ucidef_set_interface "wan" device/,+5 s/protocol "${2:-dhcp}"/protocol "${2:-pppoe}"/g' package/base-files/files/lib/functions/uci-defaults.sh
