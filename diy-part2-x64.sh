ef1f0a4ecb9680c6d701f55dd642f61b6b420fa6
lyl2dora <lyl2dora@gmail.com>
2025-03-12 15:34:25 +0800

    Update diy-part2-x64.sh

--- diy-part2-x64.sh
+++ diy-part2-x64.sh
@@ -37,4 +37,7 @@ sed -i "s/timezone='GMT0'/timezone='Asia\/Shanghai'/" package/base-files/files/b
 
 # 更加精确的替换，包含完整的函数名和上下文
 sed -i '/ucidef_set_interface "wan" device/,+5 s/protocol "${2:-dhcp}"/protocol "${2:-pppoe}"/g' package/base-files/files/lib/functions/uci-defaults.sh
+
+#修改默认shell为bash
+sed -i 's|root:x:0:0:root:/root:/bin/ash|root:x:0:0:root:/root:/bin/bash|' package/base-files/files/etc/passwd
 # 如果需要，还可以添加其他修改
