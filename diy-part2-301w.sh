6ffdc55368a3d453c140077bc1808d7aff542c99
lyl2dora <lyl2dora@gmail.com>
2025-05-01 04:51:32 +0800

    Create diy-part2-301w.sh

---
+++ diy-part2-301w.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 更精确地替换 PPPoE 的用户名和密码
+sed -i "/proto='pppoe'/,/password=/ s/username='username'/username='$PPPOE_USERNAME1'/g" package/base-files/files/bin/config_generate
+sed -i "/proto='pppoe'/,/password=/ s/password='password'/password='$PPPOE_PASSWORD1'/g" package/base-files/files/bin/config_generate
+
+# 更加精确的替换，包含完整的函数名和上下文
+sed -i '/ucidef_set_interface "wan" device/,+5 s/protocol "${2:-dhcp}"/protocol "${2:-pppoe}"/g' package/base-files/files/lib/functions/uci-defaults.sh
+
+#修改默认shell为bash
+sed -i 's|root:x:0:0:root:/root:/bin/ash|root:x:0:0:root:/root:/bin/bash|' package/base-files/files/etc/passwd
